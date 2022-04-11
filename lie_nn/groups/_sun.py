13eb3709afc4940f0f4d42b8cab640f25c3b1ed0
Ilyes Batatia <48651863+ilyes319@users.noreply.github.com>
2022-04-11 18:24:39 +0200

    clean up sun

--- lie_nn/groups/_sun.py
+++ lie_nn/groups/_sun.py
@@ -96,46 +96,15 @@ for i in range(dim((3, 2, 0))):
     assert M_to_index(index_to_M((3, 2, 0), i)) == i
 
 
-def get_egein_value(l: int, lls: List[int]):
-    np_lls = jnp.array(lls)
-    row_sum_lp1 = jnp.cumsum(np_lls[:l + 1])
-    row_sum_lm1 = jnp.cumsum(np_lls[:l - 1])
-    row_sum_l = jnp.cumsum(np_lls[:l])
-    return row_sum_l - (1 / 2) * (row_sum_lm1 + row_sum_lp1)
-
-
-def branching(weight: list[int], depth: int, pattern: dict):
-    weights = []
-    for node in range(len(weight) - 1):
-        snake_values = []
-        for j in range(weight[node] - weight[node + 1] + 1):
-            snake_values.append(j)
-        pattern[node, depth] = snake_values
-        weights.append(snake_values)
-    return weights
-
-
 @static_jax_pytree
 class SURep(AbstractRep):
-    n: int  # dimension of the SU(n) group
-    lls: List[int]  # List of weights of the representation
+    S: Tuple[int]  # List of weights of the representation
 
     def __init__(self, n):
         self.n = n
 
-    def z_weight(rep: 'SURep'):
-        return [get_egein_value(l, rep.lls) for l in range(rep.n)]
-
     def GT_patterns(rep: 'SURep'):
-        pattern = {}
-        n = rep.n
-        for i in range(n):
-            if i == 0:
-                rep = branching(rep, 1)
-        else:
-            for weights in tuple(itertools.product(*rep)):
-                rep = branching(weights, i + 1)
-        return pattern
+        return S_to_Ss(rep.S)
 
     def __mul__(rep1: 'SURep', rep2: 'SURep') -> List['SURep']:
         pass
@@ -150,14 +119,7 @@ class SURep(AbstractRep):
         # A numerical algorithm for the explicit calculation of SU(N) and SL(N, C)
         # Clebsch-Gordan coefficients Arne Alex, Matthias Kalus, Alan Huckleberry
         # and Jan von Delft Eq 22.
-        numerator = 1
-        denominator = 1
-        for i in range(self.n):
-            for sum in range(self.n):
-                j = sum - i
-                numerator *= rep.lls[j] - rep.lls[i + j] + i
-                denominator *= i
-        return numerator / denominator
+        return dim(rep.S)
 
     @classmethod
     def iterator(self, cls) -> Iterator['SURep']:
