70af3801146b1cec5bad6b59135fdecb527b6c31
Mario Geiger <geiger.mario@gmail.com>
2022-04-21 18:35:39 -0400

    fix missing coma in init

--- lie_nn/groups/__init__.py
+++ lie_nn/groups/__init__.py
@@ -1,4 +1,4 @@
-from ._abstract_rep import AbstractRep, static_jax_pytree  # noqa: F401
+from ._abstract_rep import AbstractRep, static_jax_pytree
 from ._su2 import SU2Rep
 from ._su2_real import SU2RealRep
 from ._o3_real import O3Rep
@@ -8,7 +8,8 @@ from ._so13 import SO13Rep
 from ._sun import SURep
 
 __all__ = [
-    "AbstractRep" "static_jax_pytree",
+    "AbstractRep",
+    "static_jax_pytree",
     "SU2Rep",
     "SU2RealRep",
     "O3Rep",
