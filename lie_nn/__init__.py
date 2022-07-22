b6700a9637f76db95862fdb1b37dc981c88226e8
Mario Geiger <geiger.mario@gmail.com>
2022-07-22 12:32:47 +0200

    import change basis and tensor product in init

--- lie_nn/__init__.py
+++ lie_nn/__init__.py
@@ -3,6 +3,8 @@ __version__ = "0.0.0"
 from ._rep import Rep, GenericRep
 from ._irrep import Irrep, static_jax_pytree
 from ._reduced_rep import MulIrrep, ReducedRep
+from ._change_basis import change_basis
+from ._tensor_product import tensor_product
 
 __all__ = [
     "Rep",
@@ -11,4 +13,6 @@ __all__ = [
     "static_jax_pytree",
     "MulIrrep",
     "ReducedRep",
+    "change_basis",
+    "tensor_product",
 ]
