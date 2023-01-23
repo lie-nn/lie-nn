da66eeffaa70e5b9753a20783af8ad221a776a0b
Mario Geiger <geiger.mario@gmail.com>
2023-01-23 10:50:51 -0500

    setup

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,6 @@ import nox
 
 @nox.session
 def tests(session):
-    session.install("pip", "numpy", "pytest", "ase")
+    session.install("pip", "pytest")
     session.run("pip", "install", ".", "-v")
     session.run("pytest")
