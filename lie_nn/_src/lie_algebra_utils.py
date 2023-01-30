cf75dc28423bc8b135be0feb155388058eb1f1ca
Ilyes Batatia <48651863+ilyes319@users.noreply.github.com>
2023-01-30 21:27:21 +0100

    added simple roots

--- lie_nn/_src/lie_algebra_utils.py
+++ lie_nn/_src/lie_algebra_utils.py
@@ -3,10 +3,12 @@ import numpy as np
 
 def RootsWeightsA(rank):
     epsilon = np.identity(rank + 1)
-    roots = [epsilon[i] - epsilon[i + 1] for i in range(len(epsilon) - 1)]
+    roots = [[epsilon[i] - epsilon[i + 1]] for i in range(len(epsilon) - 1)]
+    roots = np.concatenate(roots, axis=0)
     weights_1 = []
     for i in range(1, len(epsilon)):
         weights_1.append([epsilon[j] - epsilon[i] for j in range(i)])
+    weights_1 = np.concatenate(weights_1, axis=0)
     weights_2 = [sum(epsilon[j] for j in range(i)) - i / (rank + 1) * sum(epsilon) for i in range(1, rank + 1)]
     weights_3 = np.hstack([1, np.zeros(rank - 1), -1])
     return roots, weights_1, weights_2, weights_3
@@ -24,8 +26,7 @@ def RootsWeightsB(rank):
     weights_1 = np.concatenate((weights_1, epsilon))
     weights_2 = np.concatenate(([sum(epsilon[j] for j in range(i)) for i in range(1, rank)], [0.5 * sum(epsilon)]))
     weights_3 = np.concatenate(([1, 1], np.zeros(rank - 2)))
-    weight_out = np.concatenate((weights_2, np.expand_dims(weights_3, axis=0)), axis=0)
-    return roots, weights_1, weight_out
+    return roots, weights_1, weights_2, weights_3
 
 
 def RootsWeightsC(rank):
@@ -40,9 +41,7 @@ def RootsWeightsC(rank):
     weights_2 = np.concatenate((weights_1, 2 * epsilon))
     weights_3 = [sum(epsilon[j] for j in range(i)) for i in range(1, rank + 1)]
     weights_4 = np.concatenate(([2], np.zeros(rank - 1)))
-    weights_4 = np.expand_dims(weights_4, axis=0)
-    weights_out = np.concatenate((weights_3, weights_4), axis=0)
-    return roots, weights_2, weights_out
+    return roots, weights_2, weights_3, weights_4
 
 
 def RootsWeightsD(rank):
@@ -60,5 +59,29 @@ def RootsWeightsD(rank):
     else:
         weights_2 = np.concatenate((weight_sum, [0.5 * sum(epsilon) - epsilon[rank - 1]], weight_sum, [0.5 * sum(epsilon)]))
     weights_3 = np.concatenate(([1, 1], np.zeros(rank - 2)))
-    weights_3 = np.expand_dims(weights_3, axis=0)
     return roots, weights_1, weights_2, weights_3
+
+
+def simple_roots(group, rank):
+    if group == "A":
+        rootWeight = RootsWeightsA(rank)
+    elif group == "B":
+        rootWeight = RootsWeightsB(rank)
+    elif group == "C":
+        rootWeight = RootsWeightsC(rank)
+    elif group == "D":
+        rootWeight = RootsWeightsD(rank)
+    tsDim = len(rootWeight[-1])
+    SimpleRoots, PositiveRoots, FundamentalWeights, MaximalRoots = rootWeight
+    WeylVector = sum(PositiveRoots) / 2
+    CoxeterNumber = 2 * len(PositiveRoots) / rank
+    return {
+        "tsDim": tsDim,
+        "SimpleRoots": SimpleRoots,
+        "PositiveRoots": PositiveRoots,
+        "FundamentalWeights": FundamentalWeights,
+        "MaximalRoots": MaximalRoots,
+        "WeylVector": WeylVector,
+        "CoxeterNumber": CoxeterNumber,
+    }
+
